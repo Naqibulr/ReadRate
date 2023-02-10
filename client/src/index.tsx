0cbb7f491eaaf7c4556ef47b64cc29f951fba431
naqibulr <naqibulr@stud.ntnu.no>
2023-02-10 10:24:17 +0100

    #18 fix: move menu outside index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,73 +2,13 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { HashRouter, NavLink, Route } from 'react-router-dom';
-import { Carousel, Card, Container, Navbar, Nav, Dropdown, InputGroup, Form, Button } from 'react-bootstrap';
+import { Carousel, Card, Container } from 'react-bootstrap';
 import { BookAdd, BookList, BookDetails, BookEdit } from './book-components';
 import { UserDetails, UserLogIn, RegisterUser } from './user-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-
-
-class Menu extends Component {
-  render() {
-    return (
-      /*Renders navbar using components from React-Bootstrap library */
-      <Navbar 
-        collapseOnSelect
-        expand="lg"
-        variant="dark"
-        style={{ backgroundColor: 'rgb(251 238 193)',color: 'rgb(73 15 224)'}}
-      >
-        <Container >
-          {/* Container to wrap elements of navbar within given margin of page end and start */}
-
-          <Navbar.Brand href="">
-            <h3 style={{color: 'rgb(73 15 224)'}}>ReadRate</h3>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto"></Nav>
-            <Nav >
-                
-                <InputGroup className="p-3">
-                  <Dropdown id='dropdown'>
-                    <Dropdown.Toggle variant="light" id="dropdown-basic">
-                      Filter
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Genre</Dropdown.Item>
-                      <Dropdown.Item href="#/action-2">Author</Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                  
-                  <Form.Control
-                    placeholder="Search"
-                    aria-label="Search"
-                    aria-describedby="Search field"
-                  />
-                
-                  <Button variant="light" id="button-addon2">
-                    <FontAwesomeIcon icon={faSearch} />
-                  </Button>
-                  
-                </InputGroup>
-          
-              <Nav.Link href="/#/books" style={{color: 'rgb(73 15 224)'}} >Lorem ipsum</Nav.Link>
-              <Nav.Link href="/#/books/add" style={{color: 'rgb(73 15 224)'}}>Lorem ipsum</Nav.Link>
-              <Nav.Link href="/#/books/user" style={{color: 'rgb(73 15 224)'}}>Login</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+import { Menu } from './menu';
 
 class Home extends Component {
-  mounted() {
-  }
+  mounted() {}
   render() {
     return (
       <Container>
@@ -85,19 +25,10 @@ class Home extends Component {
               <Card.Body>
                 <Carousel variant="dark">
                   <Carousel.Item interval={1500}>
-                    <img
-                      className="d-block w-100"
-                      src="" 
-                      alt="First slide"
-                      height={'auto'}
-                    />
+                    <img className="d-block w-100" src="" alt="First slide" height={'auto'} />
                   </Carousel.Item>
                   <Carousel.Item interval={500}>
-                    <img
-                      className="d-block w-100"
-                      src=""
-                      alt="Second slide"
-                    />
+                    <img className="d-block w-100" src="" alt="Second slide" />
                     <Carousel.Caption style={{ color: 'white', marginBottom: '23%' }}>
                       <h3>Lorem ipsum</h3>
 
@@ -107,11 +38,7 @@ class Home extends Component {
                     </Carousel.Caption>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src=""
-                      alt="Third slide"
-                    />
+                    <img className="d-block w-100" src="" alt="Third slide" />
                     <Carousel.Caption style={{ color: 'white', marginBottom: '23%' }}>
                       <h3>Logged in yet? </h3>
                       <NavLink
@@ -138,7 +65,7 @@ ReactDOM.render(
       <Menu />
       <Route exact path="/" component={Home} />
       <Route exact path="/books" component={BookList} />
-      <Route exact path="/books/add" component={BookAdd} /> 
+      <Route exact path="/books/add" component={BookAdd} />
       <Route exact path="/books/login" component={UserLogIn} />
       <Route exact path="/books/register" component={RegisterUser} />
       <Route exact path="/books/user" component={UserDetails} />
---
+++ client/src/menu.tsx
@@ -0,0 +1,66 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Button, Container, Dropdown, Form, InputGroup, Nav, Navbar } from 'react-bootstrap';
+import { Component } from 'react-simplified';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export class Menu extends Component {
+  render() {
+    return (
+      /*Renders navbar using components from React-Bootstrap library */
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+        style={{ backgroundColor: 'rgb(251 238 193)', color: 'rgb(73 15 224)' }}
+      >
+        <Container>
+          {/* Container to wrap elements of navbar within given margin of page end and start */}
+
+          <Navbar.Brand href="">
+            <h3 style={{ color: 'rgb(73 15 224)' }}>ReadRate</h3>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <InputGroup className="p-3">
+                <Dropdown id="dropdown">
+                  <Dropdown.Toggle variant="light" id="dropdown-basic">
+                    Filter
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Genre</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Author</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+
+                <Form.Control
+                  placeholder="Search"
+                  aria-label="Search"
+                  aria-describedby="Search field"
+                />
+
+                <Button variant="light" id="button-addon2">
+                  <FontAwesomeIcon icon={faSearch} />
+                </Button>
+              </InputGroup>
+
+              <Nav.Link href="/#/books" style={{ color: 'rgb(73 15 224)' }}>
+                Lorem ipsum
+              </Nav.Link>
+              <Nav.Link href="/#/books/add" style={{ color: 'rgb(73 15 224)' }}>
+                Lorem ipsum
+              </Nav.Link>
+              <Nav.Link href="/#/books/user" style={{ color: 'rgb(73 15 224)' }}>
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
